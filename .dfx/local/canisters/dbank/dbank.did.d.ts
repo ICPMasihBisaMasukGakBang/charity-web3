da1494e7bab6fccace4c7fb878b108e35bae12ea
fajartd02 <fajarmh2@gmail.com>
2024-05-12 03:40:46 +0700

    feat: fix dfx error local run

--- .dfx/local/canisters/dbank/dbank.did.d.ts
+++
@@ -1,5 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-export interface _SERVICE {
-  'donate' : (arg_0: bigint) => Promise<bigint>,
-  'getCharityDonationMoney' : () => Promise<bigint>,
-}
--- .dfx/local/canisters/dbank/dbank.did.js
+++
@@ -1,7 +0,0 @@
-export const idlFactory = ({ IDL }) => {
-  return IDL.Service({
-    'donate' : IDL.Func([IDL.Nat], [IDL.Nat], []),
-    'getCharityDonationMoney' : IDL.Func([], [IDL.Nat], ['query']),
-  });
-};
-export const init = ({ IDL }) => { return []; };
